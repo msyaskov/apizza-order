333b8ee77f37672602635a6437f742cec7180ba6
msyaskov <ms.yaskov@gmail.com>
2024-03-05 23:00:56 +0300

    Initial commit

---
+++ src/main/java/apizza/order/ApizzaOrderApplication.java
@@ -0,0 +1,13 @@
+package apizza.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApizzaOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApizzaOrderApplication.class, args);
+    }
+
+}
