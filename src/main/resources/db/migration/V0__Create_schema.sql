1e36a4ea30e24c8d3355a30ee68a2145229c302e
msyaskov <ms.yaskov@gmail.com>
2024-03-06 00:01:09 +0300

    add: create schema migration

---
+++ src/main/resources/db/migration/V0__Create_schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE pizzas (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    price DOUBLE PRECISION NOT NULL
+);
+
+CREATE TABLE orders (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    date TIMESTAMP WITH TIME ZONE,
+    price DOUBLE PRECISION NOT NULL
+);
+
+CREATE TABLE order_pizzas (
+    order_id UUID REFERENCES orders (id),
+    pizza_id UUID REFERENCES pizzas (id)
+)
